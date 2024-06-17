f53ecefe8f5a7e4859349a1b4f44c0df8392ec05
edjay404 <ejlindayao@gmail.com>
2024-06-17 23:55:24 +0800

    Day34-Final

--- src/main/kotlin/Day34/Main.kt
+++ src/main/kotlin/Day34/Main.kt
@@ -8,7 +8,6 @@ fun cleanUserInputs(inputs: List<String?>): List<String> {
         .filter { it.isNotEmpty() }
 }
 
-// Test the function with the provided example
 fun main() {
     val inputs: List<String?> = listOf(" Alice ", null, "BOB", "  ", "  Charlie  ", null, "  dave  ", "Eve")
     val cleanedInputs: List<String> = cleanUserInputs(inputs)
